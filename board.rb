05d27db4182e8060b5b7d72686a3e87770e097a4
James Sutherland <jrsutherland78@googlemail.com>
2018-10-06 16:47:35 +0100

    feat: grid created to play on

--- board.rb
+++ board.rb
@@ -1,2 +1,27 @@
 class Board
+
+  def initialize(x_length = 5, y_length = 5)
+    @x_length = x_length
+    @y_length = y_length
+    @game_board = []
+  end
+
+  def grid
+    column = 1
+    row = 1
+    game_row = []
+
+    while column <= @x_length do
+      while row <= @y_length do
+        game_row.push(0)
+        row += 1
+      end
+      @game_board.push(game_row)
+      column += 1
+      row = 1
+      game_row = []
+    end
+    @game_board
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,12 +2,19 @@ require_relative '../board'
 
 describe "Board" do
 
-  before.each do
   board = Board.new
-  end
 
   it "a board is created at the start of a game" do
     expect(board).to be_instance_of(Board)
   end
 
+  describe "#grid" do
+    it "can create a 2D array" do
+      expect(board.grid).to eq([[0,0,0,0,0],
+                                [0,0,0,0,0],
+                                [0,0,0,0,0],
+                                [0,0,0,0,0],
+                                [0,0,0,0,0]])
+    end
+  end
 end
