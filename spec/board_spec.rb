2e08877934a7ca99bfe75d3428696034dd2330d7
James Sutherland <jrsutherland78@googlemail.com>
2018-10-06 16:31:58 +0100

    test: create a board that the game can be played on

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,13 @@
+require_relative '../board'
 
+describe "Board" do
+
+  before.each do
+  board = Board.new
+  end
+
+  it "a board is created at the start of a game" do
+    expect(board).to be_instance_of(Board)
+  end
+
+end
