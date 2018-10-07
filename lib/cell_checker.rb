9df68cdb4deb0c202bc253423bc94d3489e80869
James Sutherland <jrsutherland78@googlemail.com>
2018-10-07 20:03:44 +0100

    feat: can change a 0 to a 1 if exactly three 1's surround position

--- lib/cell_checker.rb
+++ lib/cell_checker.rb
@@ -1,8 +1,13 @@
 class CellChecker
 
   def create_frame(grid)
-    @grid = grid
-    initial_settings
+    @new_frame = Marshal.load(Marshal.dump(grid))
+
+    @x_position = 0
+    @y_position = 0
+    @neighbours = 0
+    @width = @new_frame.count - 1
+    @height = @new_frame[0].count - 1
 
     while @x_position <= @width do
       @y_position = 0
@@ -14,51 +19,45 @@ class CellChecker
         rel_num_one = - 1
         rel_num_two = - 1
 
+        num = 0
+
         while rel_num_one < 2
           rel_num_two = - 1
           while rel_num_two < 2
 
-            if @x_position + rel_num_one < 0
+            if (@x_position + rel_num_one) < 0
               x_check = @width
-            elsif @x_position + rel_num_one > @width
+            elsif (@x_position + rel_num_one) > @width
               x_check = 0
             else
-              x_check = @x_position + rel_num_one
+              x_check = (@x_position + rel_num_one)
             end
 
-            if @y_position + rel_num_two < 0
+            if (@y_position + rel_num_two) < 0
               y_check = @height
-            elsif @y_position + rel_num_two > @height
+            elsif (@y_position + rel_num_two) > @height
               y_check = 0
             else
-              y_check = @x_position + rel_num_two
+              y_check = (@y_position + rel_num_two)
             end
 
             unless @x_position == (x_check) && @y_position == (y_check)
-              @neighbours += 1 if @grid[x_check][y_check] == 1
+              @neighbours += 1 if grid[x_check][y_check] == 1
             end
             rel_num_two += 1
           end
           rel_num_one += 1
         end
         @new_frame[@x_position][@y_position] = 0 if @neighbours < 2
+        @new_frame[@x_position][@y_position] = 0 if @neighbours > 3
+        @new_frame[@x_position][@y_position] = 1 if @neighbours == 3
+        (@new_frame[@x_position][@y_position] = grid[@x_position][@y_position]) if @neighbours == 2
         @neighbours = 0
         @y_position += 1
       end
       @x_position += 1
     end
     return @new_frame
-  end
-
-  private
-
-  def initial_settings
-    @new_frame = @grid
-    @x_position = 0
-    @y_position = 0
-    @neighbours = 0
-    @width = @grid.count - 1
-    @height = @grid[0].count - 1
   end
 
 end
--- spec/cell_checker_spec.rb
+++ spec/cell_checker_spec.rb
@@ -3,6 +3,18 @@ require "cell_checker"
 describe "CellChecker" do
 
   before(:each) do
+    @test_frame_one = [[0,0,0,0,0],
+                       [0,0,0,0,0],
+                       [0,1,0,1,0],
+                       [0,0,0,0,0],
+                       [0,0,0,0,0]]
+
+    @test_frame_two = [[0,0,0,0,0],
+                       [0,0,1,0,0],
+                       [0,1,1,1,0],
+                       [0,0,1,0,0],
+                       [0,0,0,0,0]]
+
     @cell_checker = CellChecker.new
   end
 
@@ -13,15 +25,18 @@ describe "CellChecker" do
   describe "#create_frame" do
 
     it "can change a 1 to a 0 if less than two 1's surround it" do
-      @mock_frame_one = [[0,0,0,0,0],
-                         [0,0,1,0,0],
-                         [0,0,0,0,0],
-                         [0,0,1,1,0],
-                         [0,0,0,0,0]]
-      expect(@cell_checker.create_frame(@mock_frame_one)).to eq([[0,0,0,0,0],
+      expect(@cell_checker.create_frame(@test_frame_one)).to eq([[0,0,0,0,0],
                                                                  [0,0,0,0,0],
                                                                  [0,0,0,0,0],
                                                                  [0,0,0,0,0],
+                                                                 [0,0,0,0,0]])
+    end
+
+    it "can change a 0 to a 1 if exactly three 1's surround it" do
+      expect(@cell_checker.create_frame(@test_frame_two)).to eq([[0,0,0,0,0],
+                                                                 [0,1,1,1,0],
+                                                                 [0,1,0,1,0],
+                                                                 [0,1,1,1,0],
                                                                  [0,0,0,0,0]])
     end
   end
